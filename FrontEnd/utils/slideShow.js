092bd6a581875a2071aa6a5997ac2b446be63b7d
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-10-03 17:20:37 +0330

    [feat] Install Json-server

--- FrontEnd/utils/slideShow.js
+++ FrontEnd/utils/slideShow.js
@@ -5,32 +5,36 @@ const carouselIndicators = document.querySelectorAll(
 );
 let redirectTimeout; // Declare a variable to store the timeout ID
 
-//changing with the next button
+$("#myCarousel").carousel({
+  interval: false, // Disable automatic sliding
+});
+
+// Changing with the next button
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("next-btn")) {
-    currentSlide = (currentSlide + 1) % 3; // Assuming 3 slides in total
-    $("#myCarousel").carousel(currentSlide); // Move the Bootstrap carousel to the next slide
-    goToNextSlide();
+    if (currentSlide === 2) {
+      // If on the last slide, redirect
+      window.location.href = "../views/Login.html";
+    } else {
+      currentSlide = (currentSlide + 1) % 3; // Assuming 3 slides in total
+      $("#myCarousel").carousel(currentSlide); // Move the Bootstrap carousel to the next slide
+      updateBtnText();
+    }
   }
 });
 
-//changing with the indicators
+// Changing with the indicators
 carouselIndicators.forEach((indicator, index) => {
   indicator.addEventListener("click", () => {
     currentSlide = index;
-    goToNextSlide();
+    updateBtnText();
   });
 });
 
-function goToNextSlide() {
+function updateBtnText() {
   $("#myCarousel").carousel(currentSlide); // Move the Bootstrap carousel to the next slide
   if (currentSlide === 2) {
     nextButton.textContent = "Get Started";
-
-    // Set a new timeout for redirection after 4 seconds
-    setTimeout(function () {
-      window.location.href = "../views/Login.html";
-    }, 500);
   } else {
     nextButton.textContent = "Next";
   }
